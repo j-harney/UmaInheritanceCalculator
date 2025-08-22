894d079f3047dd06f496661e9aa9d64cb6dcde6d
j-harney <jpharney2022@gmail.com>
2025-08-22 14:28:38 -0400

    Finish Dictionary keys and values and Spark constructor.

--- UmaInheritanceCalculator/UmaObjects/Spark.cs
+++ UmaInheritanceCalculator/UmaObjects/Spark.cs
@@ -29,11 +29,42 @@ namespace UmaInheritanceCalculator.UmaObjects
             ["Power"] = "Blue",
             ["Wit"] = "Blue",
             ["Guts"] = "Blue",
+            ["Asahi Hai F.S."] = "White Race",
+            ["February S."] = "White Race",
+            ["Takamatsunomiya Kinen"] = "White Race",
+            ["Osaka Hai"] = "White Race",
+            ["Oka Sho"] = "White Race",
+            ["Satsuki Sho"] = "White Race",
+            ["Tenno Sho (Spring)"] = "White Race",
+            ["NHK Mile C."] = "White Race",
+            ["Victoria Mile"] = "White Race",
+            ["Japanese Oaks"] = "White Race",
+            ["Japanese Derby"] = "White Race",
+            ["Yasuda Kinen"] = "White Race",
+            ["Takarazuka Kinen"] = "White Race",
+            ["Sprinters S."] = "White Race",
+            ["Shuka Sho"] = "White Race",
+            ["Kikuka Sho"] = "White Race",
+            ["Tenno Sho (Autumn)"] = "White Race",
+            ["Queen Elizabeth II Cup"] = "White Race",
+            ["Mile Ch."] = "White Race",
+            ["Japan C."] = "White Race",
+            ["Champions C."] = "White Race",
+            ["Hanshin J.F."] = "White Race",
+            ["Hopeful S."] = "White Race",
+            ["Arima Kinen"] = "White Race",
+            ["Teio Sho"] = "White Race",
+            ["J.D. Derby"] = "White Race",
+            ["JBC L. Classic"] = "White Race",
+            ["JBC Sprint"] = "White Race",
+            ["JBC Classic"] = "White Race",
+            ["Tokyo Daishoten"] = "White Race"
         };
 
         public Spark(string SparkName, string SparkStars) 
         {
-            SparkType = "White";
+            this.SparkStars = SparkStars;
+            this.SparkType = "White";
             if (SparkName == null) throw new ArgumentNullException(nameof(SparkName));
             
             foreach (string sparkNames in Sparks.Keys)
@@ -42,7 +73,7 @@ namespace UmaInheritanceCalculator.UmaObjects
                 {
                     string localSparkType;
                     Sparks.TryGetValue(sparkNames, out localSparkType);
-                    SparkType = localSparkType;
+                    this.SparkType = localSparkType;
                 }
             }
         }
