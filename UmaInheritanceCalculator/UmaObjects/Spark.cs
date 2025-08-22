9ddbdb7d1bc32740f712620bf79396b15de5c2bc
j-harney <jpharney2022@gmail.com>
2025-08-22 12:29:21 -0400

    Start on constructor and dictionary for Spark Class

---
+++ UmaInheritanceCalculator/UmaObjects/Spark.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmaInheritanceCalculator.UmaObjects
+{
+    internal class Spark
+    {
+        public string SparkType;
+        public string SparkName;
+        public string SparkStars;
+
+        private static Dictionary<string, string> Sparks => new Dictionary<string, string>()
+        {
+            ["Turf"] = "Red",
+            ["Dirt"] = "Red",
+            ["Sprint"] = "Red",
+            ["Mile"] = "Red",
+            ["Medium"] = "Red",
+            ["Long"] = "Red",
+            ["Front Runner"] = "Red",
+            ["Pace Chaser"] = "Red",
+            ["End Closer"] = "Red",
+            ["Late Surger"] = "Red",
+            ["Stamina"] = "Blue",
+            ["Speed"] = "Blue",
+            ["Power"] = "Blue",
+            ["Wit"] = "Blue",
+            ["Guts"] = "Blue",
+        };
+
+        public Spark(string SparkName, string SparkStars) 
+        {
+            SparkType = "White";
+            if (SparkName == null) throw new ArgumentNullException(nameof(SparkName));
+            
+            foreach (string sparkNames in Sparks.Keys)
+            {
+                if (SparkName == sparkNames) 
+                {
+                    string localSparkType;
+                    Sparks.TryGetValue(sparkNames, out localSparkType);
+                    SparkType = localSparkType;
+                }
+            }
+        }
+    }
+}
